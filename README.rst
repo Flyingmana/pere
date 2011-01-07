fb2f94e83e55cca31faf37962ee6c4968478af65
flyingmana <flyingmana@googlemail.com>
2011-01-07 23:52:35 +0100

    # add installer for dependencies of pere

--- README.rst
+++ README.rst
@@ -18,3 +18,24 @@ the source. This means, even from git or svn repositories.
 
 We will provide an interface for using pere in your own projects
 if you want an individual lib dir.
+
+
+Dependencies
+============
+
+Yes, even we have a few dependencies.
+We are able to install some of them by ourself.
+Others should be already present.
+
+We need the following:
+
+- php(>=5.3)
+- svn
+- git
+
+We download the following libraries in a *private* subfolder.
+
+- `arbit-vcs_wrapper <http://arbitracker.org/vcs_wrapper.html>`_
+- `phpab <https://github.com/theseer/Autoload>`_ (autoload generator)
+- `DirectoryScanner <https://github.com/theseer/DirectoryScanner>`_ (is needed for phpab)
+
---
+++ src/install_dependencies.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * we like using clean reusable code,
+ * so we need to install a few libraries.
+ * 
+ *
+ */
+
+chdir(__DIR__);
+$librariedir = "libraries";
+passthru("rm -fr $librariedir");
+mkdir($librariedir);
+chdir($librariedir);
+
+
+$repository = "git://github.com/theseer/DirectoryScanner.git";
+$targetdir  = "DirectoryScanner";
+passthru("git clone $repository $targetdir");
+
+$repository = "http://svn.apache.org/repos/asf/incubator/zetacomponents/scripts/";
+$targetdir  = "ezc/current/scripts";
+mkdir($targetdir, 0777, true);
+passthru("svn checkout $repository $targetdir");
+
+$repository = "http://svn.apache.org/repos/asf/incubator/zetacomponents/trunk/Base/src/";
+$targetdir  = "ezc/current/Base/src";
+mkdir($targetdir, 0777, true);
+passthru("svn checkout $repository $targetdir");
+
+$repository = "http://svn.apache.org/repos/asf/incubator/zetacomponents/trunk/ConsoleTools/src/";
+$targetdir  = "ezc/current/ConsoleTools/src";
+mkdir($targetdir, 0777, true);
+passthru("svn checkout $repository $targetdir");
+
+
+
+
+$repository = "git://github.com/theseer/Autoload.git";
+$targetdir  = "phpab";
+passthru("git clone $repository $targetdir");
+
+$repository = "svn://arbitracker.org/arbit/projects/vcs_wrapper/trunk/src";
+$targetdir  = "arbit_vcs_wrapper";
+passthru("svn checkout $repository $targetdir");
+
+
+chdir("ezc/current");
+passthru("./scripts/setup-env.sh");
+
