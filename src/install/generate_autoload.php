086e16d5f3b2104c2bb1ba7a127719524cfff53b
flyingmana <flyingmana@googlemail.com>
2011-02-19 23:16:36 +0100

    # add script for generate an autoloadfile from needed libraries

---
+++ src/install/generate_autoload.php
@@ -0,0 +1,28 @@
+<?php
+
+    $librariedir = realpath( __DIR__ . '/../libraries' );
+    //remove unused duplicate interface
+    passthru('rm -fr ' . $librariedir . '/arbit_vcs_wrapper/classes/external/xml/cacheable.php');
+
+    require $librariedir . '/phpab/src/classfinder.php';
+    require $librariedir . '/phpab/src/autoloadbuilder.php';
+
+    require $librariedir . '/DirectoryScanner/src/directoryscanner.php';
+    require $librariedir . '/DirectoryScanner/src/phpfilter.php';
+    require $librariedir . '/DirectoryScanner/src/includeexcludefilter.php';
+    require $librariedir . '/DirectoryScanner/src/filesonlyfilter.php';
+
+    $scanner = new \TheSeer\Tools\DirectoryScanner;
+    $scanner->addInclude('*.php');
+
+    $finder = new \TheSeer\Tools\ClassFinder;
+
+    $found = $finder->parseMulti( $scanner( $librariedir . '/arbit_vcs_wrapper' ) );
+    $found = $finder->parseMulti( $scanner( $librariedir . '/DirectoryScanner/src' ) );
+    $found = $finder->parseMulti( $scanner( $librariedir . '/ezc' ) );
+    $found = $finder->parseMulti( $scanner( $librariedir . '/phpab/src' ) );
+
+    $ab = new \TheSeer\Tools\AutoloadBuilder( $finder->getClasses() );
+
+
+    file_put_contents( $librariedir . '/autoload.php', $ab->render() );
